338294199a79e5655a31722d9b2e585152f8a16c
Zihadul Islam <zihadul708@gmail.com>
2024-09-06 09:54:55 +0400

    tailwind css

---
+++ src/Nav.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+const Nav = () => {
+  return (
+    <nav className="text-emerald-500">
+      <h1>Hello thsi is navber</h1>
+      <ul>
+        <li className="text-emerald-500 bg-cyan-800">home</li>
+        <li>blogs</li>
+        <li>about</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
